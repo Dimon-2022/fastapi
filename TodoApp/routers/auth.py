d7c28cd5dc3f5342b655dd70657d2244dcd7d39d
dima.moisieienko <dimonmail09@gmail.com>
2026-01-18 19:44:01 +0200

    authenticating of user

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,9 +1,12 @@
-from dataclasses import Field
-
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter()
@@ -18,9 +21,30 @@ class CreateUserRequest(BaseModel):
     role: str
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+                      create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -31,21 +55,16 @@ async def create_user(create_user_request: CreateUserRequest):
         is_active=True
     )
 
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'failed authentication'
+    return 'success'
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return {'user': 'authenticated'}
