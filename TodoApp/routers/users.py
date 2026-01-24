3921da360f984d6405116d234eab647370d508a7
dima.moisieienko <dimonmail09@gmail.com>
2026-01-24 16:17:24 +0200

    made code review

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -8,6 +8,7 @@ from models import Todos, Users
 from database import SessionLocal
 from .auth import get_current_user, bcrypt_context
 from .todos import TodoRequest
+from passlib.context import CryptContext
 
 router = APIRouter(
     prefix='/users',
@@ -25,28 +26,30 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-class ChangeUserPassRequest(BaseModel):
+class UserVerification(BaseModel):
     password: str
+    new_password: str = Field(min_length=6)
 
-@router.get("/get-user-info")
-async def get_user_info(user: user_dependency, db: db_dependency):
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_user(user: user_dependency, db: db_dependency):
     if user is None:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-    requested_user = db.query(Users).filter(Users.id == user.get(id)).first()
-    if requested_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return requested_user
+        raise HTTPException(status_code=401, detail="Authentication failed")
+    return db.query(Users).filter(Users.id == user.get('id')).first()
 
-@router.put("/change-user-pass")
-async def change_user_pass(user: user_dependency, db: db_dependency, change_user_pass_request: ChangeUserPassRequest):
+
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency, db: db_dependency, user_verification: UserVerification):
     if user is None:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-    requested_user = db.query(Users).filter(Users.id == user.get(id)).first()
-    if requested_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    requested_user.hashed_password = bcrypt_context.hash(change_user_pass_request.password)
-    db.add(requested_user)
+        raise HTTPException(status_code=401, detail="Authentication failed")
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail="Error on password change")
+
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
     db.commit()
 
 
